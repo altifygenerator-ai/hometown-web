2cede9d4368d09e479752b99aac67490ae1cf883
AltifyDev <altifygenerator@gmail.com>
2026-03-28 05:34:03 -0500

    Sitemap

---
+++ app/robots.ts
@@ -0,0 +1,9 @@
+export default function robots() {
+  return {
+    rules: {
+      userAgent: "*",
+      allow: "/",
+    },
+    sitemap: "https://hometownwebservicesar.cc/sitemap.xml",
+  };
+}
---
+++ app/sitemap.ts
@@ -0,0 +1,22 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://hometownwebservicesar.cc",
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 1,
+    },
+    {
+      url: "https://hometownwebservicesar.cc/#pricing",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+    {
+      url: "https://hometownwebservicesar.cc/#portfolio",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+  ];
+}
