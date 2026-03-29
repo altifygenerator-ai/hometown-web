6d21268910693c5b0071a3cb024b0b4ea3532bae
AltifyDev <altifygenerator@gmail.com>
2026-03-29 05:14:35 -0500

    stuff fucked

--- app/sitemap.xml/route.ts
+++ app/sitemap.xml/route.ts
@@ -1,4 +1,5 @@
-<?xml version="1.0" encoding="UTF-8"?>
+export async function GET() {
+  const xml = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
   <url>
     <loc>https://hometownwebservicesar.cc/</loc>
@@ -6,4 +7,12 @@
     <changefreq>weekly</changefreq>
     <priority>1.0</priority>
   </url>
-</urlset>
+</urlset>`;
+
+  return new Response(xml, {
+    headers: {
+      "Content-Type": "application/xml",
+      "Cache-Control": "no-store",
+    },
+  });
+}
