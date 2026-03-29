2193ac1b897e97b717acf7a95250a4d12eb8724c
AltifyDev <altifygenerator@gmail.com>
2026-03-29 04:44:15 -0500

    sitemap: update sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,22 +1,12 @@
-export default function sitemap() {
+import { MetadataRoute } from "next";
+
+export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
       url: "https://hometownwebservicesar.cc",
       lastModified: new Date(),
       changeFrequency: "weekly",
       priority: 1,
-    },
-    {
-      url: "https://hometownwebservicesar.cc/#pricing",
-      lastModified: new Date(),
-      changeFrequency: "monthly",
-      priority: 0.8,
-    },
-    {
-      url: "https://hometownwebservicesar.cc/#portfolio",
-      lastModified: new Date(),
-      changeFrequency: "monthly",
-      priority: 0.8,
     },
   ];
 }
