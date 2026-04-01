b43b1593b0628a62ed896ba34800d54692afb93a
AltifyDev <altifygenerator@gmail.com>
2026-04-01 05:23:03 -0500

    update robots.txt and sitemap.xml

--- app/robots.ts
+++ app/robots.ts
@@ -1,9 +1,10 @@
-export default function robots() {
+import type { MetadataRoute } from 'next'
+
+const siteUrl = process.env.SITE_URL ?? 'https://hometownwebservicesar.cc'
+
+export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: "*",
-      allow: "/",
-    },
-    sitemap: "https://hometownwebservicesar.cc/sitemap.xml",
-  };
+    rules: { userAgent: '*', allow: '/' },
+    sitemap: `${siteUrl}/sitemap.xml`,
+  }
 }
